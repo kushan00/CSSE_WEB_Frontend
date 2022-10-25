63b973ef64cf5c1bef6f646055d9702b364e76e9
Kushan Dimantha <kushandimantha533@gmail.com>
2022-10-25 23:47:08 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.scss';
+import 'boxicons/css/boxicons.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AppLayout from './components/layout/AppLayout';
+import Blank from './pages/Blank';
+import Home from './pages/Home';
+import Logout from './pages/Auth/logout';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<AppLayout />}>
+                    <Route index element={<Home />} />
+                    <Route path='/profile' element={<Home />} />
+                    <Route path='/received-prs' element={<Blank />} />
+                    <Route path='/items-catalogue' element={<Blank />} />
+                    <Route path='/view-prs' element={<Blank />} />
+                    <Route path='/site-details' element={<Blank />} />
+                    <Route path='/suppliers' element={<Blank />} />
+                    <Route path='/logout' element={<Logout />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,116 @@
+import { useEffect, useRef, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './sidebar.scss';
+
+const sidebarNavItems = [
+    {
+        display: 'Home',
+        icon: <i className='bx bx-home'></i>,
+        to: '/',
+        section: ''
+    },
+    {
+        display: 'Profile',
+        icon: <i className='bx bx-star'></i>,
+        to: '/profile',
+        section: 'profile'
+    },
+    {
+        display: 'Received PRs',
+        icon: <i className='bx bx-calendar'></i>,
+        to: '/received-prs',
+        section: 'received-prs'
+    },
+    {
+        display: 'Items Catalogue',
+        icon: <i class='bx bxs-receipt' ></i>,
+        to: '/items-catalogue',
+        section: 'items-catalogue'
+    },
+    {
+        display: 'View PRs',
+        icon: <i class='bx bxs-file-find' ></i>,
+        to: '/view-prs',
+        section: 'view-prs'
+    },
+    {
+        display: 'Site Details',
+        icon: <i className='bx bx-detail'></i>,
+        to: '/site-details',
+        section: 'site-details'
+    },
+    {
+        display: 'Suppliers',
+        icon: <i className='bx bxs-truck'></i>,
+        to: '/suppliers',
+        section: 'suppliers'
+    },
+    {
+        display: 'logout',
+        icon: <i class='bx bxs-chevron-left-circle' ></i>,
+        to: '/logout',
+        section: 'logout'
+    },
+]
+
+const Sidebar = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [stepHeight, setStepHeight] = useState(0);
+    const sidebarRef = useRef();
+    const indicatorRef = useRef();
+    const location = useLocation();
+
+    useEffect(() => {
+        setTimeout(() => {
+            const sidebarItem = sidebarRef.current.querySelector('.sidebar__menu__item');
+            indicatorRef.current.style.height = `${sidebarItem.clientHeight}px`;
+            setStepHeight(sidebarItem.clientHeight);
+        }, 50);
+    }, []);
+
+    // change active index
+    useEffect(() => {
+        const curPath = window.location.pathname.split('/')[1];
+        const activeItem = sidebarNavItems.findIndex(item => item.section === curPath);
+        setActiveIndex(curPath.length === 0 ? 0 : activeItem);        
+    }, [location]);
+
+    return (
+    <div className='sidebar'>
+        <div className="sidebar__logo">
+            <center style={{color:"purple"}}>
+            Procurement 
+            <br/>
+            For
+            <br/>
+            Construction Industry
+            </center>
+        </div>
+        <div ref={sidebarRef} className="sidebar__menu">
+            <div
+                ref={indicatorRef}
+                className="sidebar__menu__indicator"
+                style={{
+                    transform: `translateX(-50%) translateY(${activeIndex * stepHeight}px)`
+                }}
+            ></div>
+            { 
+                sidebarNavItems.map((item, index) => (                                 
+                    <Link to={item.to} key={index}>
+                        <div className={`sidebar__menu__item ${activeIndex === index ? 'active' : ''}`}>
+                            <div className="sidebar__menu__item__icon">
+                                {item.icon}
+                            </div>
+                            <div className="sidebar__menu__item__text">
+                                {item.display}
+                            </div>
+                        </div>
+                    </Link>
+                ))
+            } 
+        </div>
+    </div>
+    );
+};
+
+export default Sidebar;
