20642c60aeaaf9c1eed407067ae6a4561e5c4ba1
Kushan Dimantha <kushandimantha533@gmail.com>
2022-10-27 11:16:33 +0530

    user authentication & autharization

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import './App.scss';
 import 'boxicons/css/boxicons.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthContextProvider } from "./context/Auth.context.js"; 
+import axios from "axios";
 import AppLayout from './components/layout/AppLayout';
 import Blank from './pages/Blank';
 import Home from './pages/Home';
 import Logout from './pages/Auth/logout';
+import Login from './pages/Auth/Login';
+
+axios.defaults.withCredentials = true;
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<AppLayout />}>
-                    <Route index element={<Home />} />
-                    <Route path='/profile' element={<Home />} />
-                    <Route path='/received-prs' element={<Blank />} />
-                    <Route path='/items-catalogue' element={<Blank />} />
-                    <Route path='/view-prs' element={<Blank />} />
-                    <Route path='/site-details' element={<Blank />} />
-                    <Route path='/suppliers' element={<Blank />} />
-                    <Route path='/logout' element={<Logout />} />                    
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <AuthContextProvider>
+                <BrowserRouter>
+                    
+                    <Routes>
+                        <Route path='/login' element={<Login />} />
+                        <Route path='/' element={<AppLayout />}>
+                            <Route index element={<Home />} />
+                            <Route path='/profile' element={<Home />} />
+                            <Route path='/received-prs' element={<Blank />} />
+                            <Route path='/items-catalogue' element={<Blank />} />
+                            <Route path='/view-prs' element={<Blank />} />
+                            <Route path='/site-details' element={<Blank />} />
+                            <Route path='/suppliers' element={<Blank />} />
+                            <Route path='/logout' element={<Logout />} />                    
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+        </AuthContextProvider>
+        
     );
 }
 
